18d8a74ade2beadae2dd843c83bf5f4674b130d9
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-09-28 20:04:23 +0200

    add taasks

---
+++ MatchingBrackets/Program.cs
@@ -0,0 +1,24 @@
+﻿string expression = Console.ReadLine();
+
+var stack = new Stack<int>();
+
+for (int i = 0; i < expression.Length; i++)
+{
+    var symbol = expression[i];
+
+    if (symbol == '(')
+    {
+        stack.Push(i);
+    }
+
+    if (symbol == ')')
+    {
+        var openingBracketIndex = stack.Pop();
+        var closingBracketIndex = i;
+
+        var resultExpression = expression.Substring(openingBracketIndex, closingBracketIndex - openingBracketIndex + 1);
+        Console.WriteLine(resultExpression);
+    }
+}
+
+
--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿string[] tokens = Console.ReadLine().Split().Reverse().ToArray();
+
+var stack = new Stack<string>(tokens);
+
+int sum = 0;
+bool isAddition = true;
+
+while (stack.Count > 0)
+{
+    var currentToken = stack.Pop();
+
+    if (currentToken == "+")
+    {
+        isAddition = true;
+    }
+    else if (currentToken == "-")
+    {
+        isAddition = false;
+    }
+    else
+    {
+        int number = int.Parse(currentToken);
+
+        if (!isAddition)
+        {
+            number *= -1;
+        }
+
+        sum += number;
+    }
+}
+
+Console.WriteLine(sum);
