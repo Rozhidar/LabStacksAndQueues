bacb28568817584930ab8bf56ef37dfd8e3554fb
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-10-03 13:23:10 +0200

    add last project

---
+++ TrafficJam/Program.cs
@@ -0,0 +1,35 @@
+﻿int passingVehicles = int.Parse(Console.ReadLine());
+
+int passedCars = 0;
+
+var queue = new Queue<string>();
+
+while (true)
+{
+    string command = Console.ReadLine();
+
+    if (command == "end")
+    {
+        Console.WriteLine($"{passedCars} cars passed the crossroads.");
+        break;
+    }
+    else if (command == "green")
+    {
+        for (int i = 0; i < passingVehicles; i++)
+        {
+            if (queue.Any())
+            {
+                Console.WriteLine($"{queue.Dequeue()} passed!");
+                passedCars++;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+    else
+    {
+        queue.Enqueue(command);
+    }
+}
